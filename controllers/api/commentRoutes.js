4cd75d416b8f0059d1d8401380db5c164e726566
AJoelj84 <ajoelj84@gmail.com>
2023-06-26 13:19:21 -0400

    working on get routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -15,3 +15,19 @@ router.get ('/', async (req, res) => {
                 res.status(500).json(err);
         }      
     });
+
+router.get ('/:id', async (req,res) => {
+    try {
+            const comData = await Comment.findAll({
+                where: { id: req.params.id },
+            });
+    if ( comData.length === 0 ) {
+        res.status(404).json ({ message: 'No Comment with id ${req.params.it}' });
+        return;
+        }
+        res.status(200).json(comData);
+        }
+        catch (err) {
+        res.status(500).json(err);
+        }
+    });
