fe4244891a0bcd58c8aa8c699e4f78803baa8c66
AJoelj84 <ajoelj84@gmail.com>
2023-06-28 15:43:04 -0400

    reverted nonworking changes

--- server.js
+++ server.js
@@ -31,7 +31,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    require('./seeds/seed')();
-}).then(()=>{
     app.listen(PORT, () => console.log('Now listening on port ' + PORT));
   });
