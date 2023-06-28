923d26a9b33f9e4b4a31759ea2a55eee12e14b44
AJoelj84 <ajoelj84@gmail.com>
2023-06-28 14:56:50 -0400

    made changes to connection.js for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,16 @@
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
 
 if (process.env.DATABASE_URL) {
-  
-  console.log('DATABASE_URL:', process.env.DATABASE_URL);
-  const urlParts = new URL(process.env.DATABASE_URL);
-  console.log('urlParts:', urlParts);
-
-
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'mysql',
-    logging: false
-  });
+  sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST || 'localhost',
-      dialect: 'mysql',
-      port: process.env.DB_PORT || 3306
-    }
-  );
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3306
+  });
 }
 
 module.exports = sequelize;
