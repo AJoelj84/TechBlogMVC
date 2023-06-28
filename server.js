808066d27b1b0f1343f01e98e2eca771362badef
AJoelj84 <ajoelj84@gmail.com>
2023-06-28 15:41:45 -0400

    changes to get heroku to run seeds

--- server.js
+++ server.js
@@ -31,5 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
+    require('./seeds/seed')();
+}).then(()=>{
     app.listen(PORT, () => console.log('Now listening on port ' + PORT));
   });
